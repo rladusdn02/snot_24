6ede102ffa9b76cabc8d1506fd76a47a8749bddc
yeon <rladusdn56@gmail.com>
2024-10-29 20:56:54 +0900

    카펫 시간 줄이기

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,14 +1,16 @@
 class Solution {
     public int[] solution(int brown, int yellow) {
         int[] answer = new int[2];
-        int carpet = brown+yellow; //전체 카펫 면적
-        for (int col = 3; col<carpet; col++){ //높이가 3일때부터 탐색시작
-            if(carpet%col == 0){ //세로로 나누어떨어질때만
-                int row = carpet/col; //가로 구하기
-                if((row-2)*(col-2) == yellow){ //테두리만큼 뺀경우가 yellow일때만
-                    //답 저장
-                    answer[1]=row;
-                    answer[0]=col;
+        int cell = brown + yellow; // 전체 카펫
+
+        for (int col = 3; col <= Math.sqrt(cell); col++) { // col은 카펫 높이, 카펫의 루트까지만 탐색
+            if (cell % col == 0) { // 세로로 나누어 떨어질 때만
+                int row = cell / col; // 가로 구하기
+                if ((row - 2) * (col - 2) == yellow) { // 테두리만큼 뺀 경우가 yellow일 때만
+                    // 답 저장
+                    answer[0] = row;
+                    answer[1] = col;
+                    break; // 조건을 만족했으므로 탐색 종료
                 }
             }
         }
